66eadd52dcd4f42f33f028995ded69dbf2ab2b58
kandhati <128295858+kandhati@users.noreply.github.com>
2023-10-06 10:07:41 +0100

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Welcome to Springboot Application";
 	}
 
 	public static void main(String[] args) {
